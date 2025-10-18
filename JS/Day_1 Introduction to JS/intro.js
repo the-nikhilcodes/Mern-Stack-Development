1a7f653cc703197e032e158da30d960256069120
Nikhil <nikhilgithub2005@gmail.com>
2025-10-18 21:10:18 +0530

    Hellow Java script , today i take a intro to javascript

---
+++ JS/Day_1 Introduction to JS/intro.js
@@ -0,0 +1,19 @@
+alert("Hello World!");
+
+console.log("Hello World! This is my first JS code.");
+console.log("Hello World! This is my second JS code.");
+
+var a=prompt("enter a number")
+var istrue = prompt("Are you sure you want to leave this page? and blast your computer")
+
+if (istrue) {
+    alert("You chose to leave the page. Goodbye!");
+} else {
+    alert("You chose to stay on the page. Welcome back!");
+}       
+
+console.log("The number you entered is " + a);
+
+document.title="My First JS Code"
+
+// document.body.style.backgroundColor = "lightblue";
---
+++ JS/Day_1 Introduction to JS/new.js
@@ -0,0 +1,3 @@
+
+console.log("Hello World! This is my first JS code.");
+console.log("Hello World! This is my second JS code.");
