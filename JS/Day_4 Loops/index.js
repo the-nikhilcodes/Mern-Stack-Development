023a1ce422dd21d7448a030efeaf4898da0ce5e4
Nikhil <nikhilgithub2005@gmail.com>
2025-10-23 23:12:07 +0530

    added the loops in js with program

---
+++ JS/Day_4 Loops/index.js
@@ -0,0 +1,36 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+console.log(a)
+console.log(a+1)
+console.log(a+2)
+
+for (let i = 0; i < 100; i++) { 
+    console.log(a + i); 
+}
+
+let obj = {
+    name: "Nikhil",
+    role: "Programmer",
+    company: "Nikhil AI"
+}
+ 
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "Nikhil") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
