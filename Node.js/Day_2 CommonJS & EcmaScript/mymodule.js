d11d4bbd0cecbfa5db407f20c2f867d95fbd4528
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-11-12 20:30:21 +0530

    Add CommonJS and ES module examples -   including a basic Node.js server, ES module exports,
    Introduced files demonstrating CommonJS and EcmaScript module usage, including a basic Node.js server, ES module exports, and CommonJS module exports. Added supporting HTML and package files for setup.

---
+++ Node.js/Day_2 CommonJS & EcmaScript/mymodule.js
@@ -0,0 +1,12 @@
+export const a = 11; // named export
+export const b = 22
+export const c = 33; 
+export const d = 44; 
+export const e = 55; 
+
+const obj = {
+  x: 10,
+  y: 70,
+};
+
+export default obj; // default export
